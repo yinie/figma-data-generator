7665b61f2fc7b1b3efe4025da6277f1989f5ee4c
Yi Nie <yinie@Yi.N-MBPro>
2020-05-20 14:46:26 -0700

    Move functions to webpack

--- src/code.ts
+++ src/code.ts
@@ -1,20 +1,161 @@
-figma.showUI(__html__)
+// This plugin will open a modal to prompt the user to enter a number, and
+// it will then create that many rectangles on the screen.
 
+// This file holds the main code for the plugins. It has access to the *document*.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser enviroment (see documentation).
+
+// This shows the HTML page in "ui.html".
+
+figma.showUI(__html__);
+figma.ui.resize(320,300);
+
+
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
 figma.ui.onmessage = msg => {
-  if (msg.type === 'create-rectangles') {
-    const nodes = []
-
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle()
-      rect.x = i * 150
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}]
-      figma.currentPage.appendChild(rect)
-      nodes.push(rect)
-    }
-
-    figma.currentPage.selection = nodes
-    figma.viewport.scrollAndZoomIntoView(nodes)
+  // One way of distinguishing between different types of messages sent from
+  // your HTML page is to use an object with a "type" property like this.
+  if (msg.type === 'get-count') {
+    const node = figma.currentPage.selection; 
+    const selectcount = node.length;
+    figma.ui.postMessage(selectcount);
   }
 
-  figma.closePlugin()
-}
+  if (msg.type === 'create-rectangles') {
+    
+    const node = figma.currentPage.selection; 
+    const SelectCount = node.length;
+    const MerchantData = msg.data;
+    for (let i = 0; i < SelectCount; i++){
+      FindandFillImage(node[i],"#HeroImage", MerchantData[i].HeroImage);
+      FindandFillImage(node[i],"#MerchantLogo", MerchantData[i].Logo);
+      FindandFill(node[i],'#MerchantName',MerchantData[i].name);
+    }
+  
+
+    
+    //const newFills = [];
+        //const newPaint = JSON.parse(JSON.stringify(node.fills[0]))
+       // const imageHash = figma.createImage(msg.data).hash
+       // newFills.push({ type: "IMAGE", scaleMode: "FIT", imageHash })
+       // console.log(newFills)
+      
+      
+     // node.fills = newFills
+   
+
+  }//End of create-rectangles 
+
+  if (msg.type === 'text-change'){
+    const node = figma.currentPage.selection; 
+    const principle =msg.principle;
+    const term = msg.term;
+    const apr = msg.apr;
+    const monthlypayment = MonthlyPayment(principle,term,apr);
+    const total = term * monthlypayment;
+    const interest = total - principle;
+    console.log("pinciple is", principle, "term is", term, "apr is", apr, "Monthlypayment", monthlypayment);
+
+    
+    //Fill values for term cards
+    node.forEach((element) =>{
+      FindandFill(element,'#APR',apr);
+      FindandFill(element,'#MonthlyPayment',monthlypayment);
+      FindandFill(element,'#Total',total);
+      FindandFill(element,'#Interest',interest);
+      FindandFill(element,'#Term',term);
+      FindandFill(element,'#TermMonth',term);
+    })
+
+    //Fill term card with relevant number
+    node.forEach((element)=>{
+      FindandChangeColor(element,'#TermColor',term);
+      FindandChangeColor(element,'/ month',term);
+      FindandChangeColor(element,'#MonthlyPayment',term);
+      FindandChangeColor(element,'#TermMonth',term);
+    })
+
+  }  // End of text-change msg 
+
+  
+  //Function to calculate monthly payment
+  function MonthlyPayment(princicle, termInMonth, rate) {
+    const monthlyRate = rate*30 /366/100 + 1;
+    let sumEachMonthRate = 0;
+    for (let i = 0; i < termInMonth - 1; i++) {
+      sumEachMonthRate += Math.pow(monthlyRate, i);
+    }
+    const MP = princicle * Math.pow(monthlyRate, termInMonth) / (sumEachMonthRate + 1);
+    return MP
+  }
+
+  //Given a certain layer name, fill the desired content 
+  function FindandFill (range, LayerName,FillValue){
+    if(range.type != 'RECTANGLE'){
+      const Nodes = range.findAll(range => range.type === "TEXT" && range.name === LayerName)
+      Nodes.forEach(async (element) => {
+        if (element.type === "TEXT" ){
+          await figma.loadFontAsync(element.fontName as FontName)
+          if(LayerName === '#APR'){
+            element.characters = `${FillValue}%`;
+          }else if (LayerName === '#Term' || LayerName === "#TermMonth"){
+            element.characters = `${FillValue} month`;
+          }else if (LayerName ==='#MonthlyPayment' || LayerName ==='#Interest' || LayerName ==='#Total'){
+            element.characters = `$${FillValue.toFixed(2)}`;
+          }else{
+            element.characters = FillValue;
+          }
+        }
+      })
+    }
+  } // End of FindandFill function
+
+  function FindandChangeColor(range, LayerName,term){
+    const Colornodes = range.findAll(range => range.name === LayerName);
+    Colornodes.forEach((element) =>{
+      if (element.type == "RECTANGLE" || element.type === "TEXT"  ){
+        const fills = clone(element.fills)
+        console.log(term)
+        if (term == 3){
+          fills[0].color = {r: 15/255, g: 156/255, b: 237/255};
+        }else if (term == 6){
+          fills[0].color = {r: 15/255, g: 114/255, b: 229/255};
+        }else{
+          fills[0].color = {r: 88/255, g: 70/255, b: 228/255};
+        }
+        element.fills = fills;
+      }
+    })
+  }
+
+  function FindandFillImage(range,LayerName,ImageData){
+    const newFills = [];
+    const node = range.findOne(range =>  range.name === LayerName)
+    console.log(node);
+    if (node){
+      if (node.type === "RECTANGLE" || node.type === "ELLIPSE" || node.type === "FRAME") {
+        const imageHash = figma.createImage(ImageData).hash
+        newFills.push({ type: "IMAGE", scaleMode: "FIT", imageHash })
+        node.fills = newFills;
+      }
+    }
+    //const newPaint = JSON.parse(JSON.stringify(node.fills[0]))
+  }
+  
+  function clone(val) {
+    return JSON.parse(JSON.stringify(val))
+  }
+
+
+
+
+  // Make sure to close the plugin when you're done. Otherwise the plugin will
+  // keep running, which shows the cancel button at the bottom of the screen.
+ //figma.closePlugin();
+};   
+
+
+
+
--- src/ui.ts
+++ src/ui.ts
@@ -3,12 +3,68 @@ import {MERCHANT_LIST} from './data.js'
 
 console.log(MERCHANT_LIST);
 
-document.getElementById('create').onclick = () => {
-  const textbox = document.getElementById('count') as HTMLInputElement
-  const count = parseInt(textbox.value, 10)
-  parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*')
+document.querySelector('#cancel').addEventListener('click', () => {
+  parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
+})
+
+document.querySelector('.tab-item[data-tab-1]').addEventListener('click', () => {
+  openTab('term-card');
+})
+document.querySelector('.tab-item[data-tab-2]').addEventListener('click', () => {
+  openTab('merchant-card');
+})
+document.querySelector('.tab-item[data-tab-3]').addEventListener('click', () => {
+  openTab('cool-stuff');
+})
+
+function getInputElement(selector) {
+  return(document.querySelector(selector) as HTMLInputElement);
 }
 
-document.getElementById('cancel').onclick = () => {
-  parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
+document.querySelector('#fill').addEventListener('click', () => {
+  const principle = getInputElement('#principle').value;
+  const term = getInputElement('#term').value;
+  const apr = getInputElement('#apr').value;
+  parent.postMessage({ pluginMessage: { type: 'text-change',principle,term,apr } }, '*')
+})
+
+document.querySelector('#create').addEventListener('click', () => {
+  //Get count of selection
+  parent.postMessage({ pluginMessage: { type: 'get-count'} }, '*')
+  onmessage = async (event) => {
+    //console.log("got this from the plugin code", event.data.pluginMessage)
+    const SelectionCount = event.data.pluginMessage;
+    console.log(SelectionCount)
+    MERCHANT_LIST.sort(() => Math.random() - .5);
+    let result = [];
+    for (let i = 0; i < SelectionCount ; i++){
+      console.log(i);
+      result.push({
+        name: MERCHANT_LIST[i].Name,
+        HeroImage: await getAnImage(MERCHANT_LIST[i].HeroImageURL),
+        Logo: await getAnImage(MERCHANT_LIST[i].LogoURL),
+      });
+    }
+    parent.postMessage({ pluginMessage: { type: 'create-rectangles', data: result} }, '*')
+  }
+})
+
+async function getAnImage(url) {
+  console.log(url)
+  const r = await fetch(url);
+  if((r.status+"")[0] != "2") throw Error ('error')
+  const arrayBuffer = await r.arrayBuffer()
+  return new Uint8Array(arrayBuffer);
+}
+
+function openTab(tabName) {
+  var i;
+  var x = Array.from(document.querySelectorAll(".tab-page")) as HTMLElement[];
+  var y= document.querySelectorAll(".tab-item")
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+    y[i].classList.remove("tab-item-active");
+  }
+  document.getElementById(tabName).style.display = "block";  
+  (event.target as HTMLElement).classList.add("tab-item-active");
 }
